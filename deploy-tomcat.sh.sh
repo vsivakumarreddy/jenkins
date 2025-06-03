a9b49af06982376449d2df5bdb94c6db4cc58d69
manikiran7 <manikiranreddy7@gmail.com>
2025-06-03 17:37:24 +0530

    added

--- deploy-tomcat.sh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TOMCAT_URL="http://3.86.140.185:8080/manager/html"
-TOMCAT_USER="your-tomcat-user"
-TOMCAT_PASS="your-tomcat-pass"
-WAR_FILE="target/SimpleCustomerApp.war"
-
-curl -u $TOMCAT_USER:$TOMCAT_PASS -T $WAR_FILE "$TOMCAT_URL/deploy?path=/SimpleCustomerApp&update=true"
